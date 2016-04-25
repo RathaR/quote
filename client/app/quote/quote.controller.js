6016192c6abf75049c1ce676a28f286ea423c94c
RathaR <ivan.pecharitsa@gmail.com>
2016-04-25 22:18:35 +0300

    implement voting

--- client/app/quote/quote.controller.js
+++ client/app/quote/quote.controller.js
@@ -11,9 +11,32 @@
     $onInit() {
       var id = this.id = this.opts.id;
       this.quoteService.one(id).then((quote) => {
-          this.text = quote.text;
-          this.rating = quote.rating;
-        })
+        this.text = quote.text;
+        this.rating = quote.rating;
+        this.voted = quote.voted;
+      })
+    }
+
+    vote() {
+      if (this.voted) {
+        return;
+      }
+      var id = this.id;
+      this.quoteService.vote(id).then(()=> {
+        this.voted = true;
+        this.rating++;
+      })
+    }
+
+    unVote() {
+      if (this.voted) {
+        return;
+      }
+      var id = this.id;
+      this.quoteService.unVote(id).then(()=> {
+        this.voted = true;
+        this.rating--;
+      })
     }
   }
 
